028a059817fe0299ebaed82abb631a952b921b02
海波 <1091675746@qq.com>
2020-05-09 17:23:39 +0800

    ceshi

--- test/XUnitTestProject1/UnitTest1.cs
+++ test/XUnitTestProject1/UnitTest1.cs
@@ -5,8 +5,12 @@ namespace XUnitTestProject1
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+
+        [Theory]
+        [InlineData(0,1)]
+        [InlineData(1, 2)]
+        [InlineData(2, 3)]
+        public void Test1(int i, int j)
         {
 
         }
