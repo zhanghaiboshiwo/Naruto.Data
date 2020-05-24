77861756c51a490dc9d6c64ae690dcd73bd6de85
也许所谓 <1091675746@qq.com>
2020-05-24 13:02:04 +0800

    加入wehereif

--- src/Naruto.MongoDB/MongoQueryableExtension.cs
+++ src/Naruto.MongoDB/MongoQueryableExtension.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace MongoDB.Driver.Linq
@@ -22,6 +23,23 @@ namespace MongoDB.Driver.Linq
             if (page < 1)
                 page = 1;
             return @this.Skip((page - 1) * pageSize).Take(pageSize);
+        }
+
+      /// <summary>
+      /// 
+      /// </summary>
+      /// <typeparam name="T"></typeparam>
+      /// <param name="this"></param>
+      /// <param name="condition"></param>
+      /// <param name="where"></param>
+      /// <returns></returns>
+        public static IMongoQueryable<T> WhereIf<T>(this IMongoQueryable<T> @this, bool condition, Expression<Func<T, bool>> where)
+        {
+            if (@this == null)
+                return default;
+            return condition
+                ? @this.Where(where)
+                : @this;
         }
     }
 }
